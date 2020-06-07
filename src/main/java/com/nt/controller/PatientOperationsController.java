928661a46c5ba9a61c59ef046b0131f9f5ccd075
amyprojectgithub <62251946+amyprojectgithub@users.noreply.github.com>
2020-06-07 22:12:15 +0530

    log added

--- src/main/java/com/nt/controller/PatientOperationsController.java
+++ src/main/java/com/nt/controller/PatientOperationsController.java
@@ -26,7 +26,7 @@ public class PatientOperationsController {
 
 	@ModelAttribute("patCmd")
 	public PatientCommand getPatientCommand() {
-		System.out.println("PatientOperationsController.getPatientCommand()");
+		System.out.println(" Inside PatientOperationsController.getPatientCommand()");
 		return new PatientCommand();
 	}
 
